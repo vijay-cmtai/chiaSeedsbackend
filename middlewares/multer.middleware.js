8e1166eec673074046735cbf25380318db92d35c
vijay-cmtai <vijay.maurya@completrix.com>
2025-07-10 14:45:19 +0530

    Update multer.middleware.js

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,27 +1,10 @@
-// src/middlewares/multer.middleware.js
-
 import multer from "multer";
-import fs from "fs";
-import path from "path";
-
-const tempDir = "./public/temp";
-
-// Ensure the temporary directory exists
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, tempDir);
+    cb(null, "/tmp");
   },
   filename: function (req, file, cb) {
-    // Use a unique filename to avoid conflicts, but keep the original extension
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
+    cb(null, file.originalname);
   },
 });
 
