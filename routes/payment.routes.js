24f11297159728636e352192dbbd8e6decfde3e0
vijay-cmtai <vijay.maurya@completrix.com>
2025-07-16 08:40:43 +0530

    Update payment.routes.js

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -3,10 +3,17 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 import {
   createRazorpayOrder,
   verifyPaymentAndPlaceOrder,
+  cancelOrder,
 } from "../controllers/payment.controller.js";
 
 const router = Router();
+
 router.use(authMiddleware);
+
 router.route("/create-order").post(createRazorpayOrder);
+
 router.route("/verify").post(verifyPaymentAndPlaceOrder);
+
+router.route("/:orderId/cancel").patch(cancelOrder);
+
 export default router;
