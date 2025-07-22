c7fb6c1862aa5215af48fe102d50fdbd20769153
vijay-cmtai <vijay.maurya@completrix.com>
2025-07-22 13:09:04 +0530

    Update payment.routes.js

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,18 +1,33 @@
+// your-project/backend/src/routes/payment.routes.js
+
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import {
   createRazorpayOrder,
   verifyPaymentAndPlaceOrder,
   cancelOrder,
+  getMyOrders,
+  getSingleOrderDetails,
+  retryShipment,
+  getPriceBreakdown, // Naye function ko import karein
 } from "../controllers/payment.controller.js";
 
 const router = Router();
 
 router.use(authMiddleware);
 
+// YEH HAI NAYA ROUTE
+router.route("/price-breakdown").post(getPriceBreakdown);
+
 router.route("/create-order").post(createRazorpayOrder);
 
 router.route("/verify").post(verifyPaymentAndPlaceOrder);
+
+router.route("/my-orders").get(getMyOrders);
+
+router.route("/retry-shipment").post(retryShipment);
+
+router.route("/:orderId").get(getSingleOrderDetails);
 
 router.route("/:orderId/cancel").patch(cancelOrder);
 
