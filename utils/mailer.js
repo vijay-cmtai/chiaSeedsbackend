1c5c4faa33891447116e7cf75fc4c3493a5db51f
vijay-cmtai <vijay.maurya@completrix.com>
2025-07-30 13:46:25 +0530

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,29 +1,24 @@
 import nodemailer from "nodemailer";
 import { ApiError } from "./ApiError.js";
 
-// Setup transporter for Zoho SMTP
 const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.com",
-  port: 465,            // SSL port for secure Zoho SMTP
-  secure: true,         // true for port 465
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
   auth: {
-    user: process.env.MAIL_USER,    // e.g., hello@naraaglobal.com
-    pass: process.env.MAIL_PASS,    // App password (not login password)
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
-  logger: true,         // optional: show debug logs in console
-  debug: true,          // optional: detailed output for debugging
 });
 
 const sendEmail = async (to, subject, html) => {
   try {
-    const info = await transporter.sendMail({
+    await transporter.sendMail({
       from: `"Naraaglobal Enterprises" <${process.env.MAIL_USER}>`,
-      to,
-      subject,
-      html,
+      to: to,
+      subject: subject,
+      html: html,
     });
-
-    console.log("Email sent: %s", info.messageId);
   } catch (error) {
     console.error("Error sending email:", error);
     throw new ApiError(
